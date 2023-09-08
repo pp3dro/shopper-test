af51df6871325c005ea29d7e8a488f10159edf66
Pedro <pedroharib@gmail.com>
2023-09-08 13:37:39 -0300

    correção text colors no component Alert.tsx e configuração de variável de ambiente

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    env: {
-        NEXT_PUBLIC_APP_PRODUCT_API: 'http://localhost:3000/products'
-    }
 }
 
 module.exports = nextConfig
--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -7,15 +7,18 @@ interface AlertProps {
 }
 
 export const Alert = ({message, type, close}: AlertProps) => {
-    const color = type === 'error' ? 'red' : 'green';
+    const textColor = type === 'error' ? 'text-red-700' : 'text-green-500';
+    const borderColor = type === 'error' ? 'border-red-400' : 'border-green-400';
+    const bgColor = type === 'error' ? 'bg-red-100' : 'bg-green-100';
+    
     return message ? (
-        <div className={`flex flex-row items-center justify-center w-full mb-5 bg-${color}-100 border border-${color}-400 text-${color}-700 px-4 py-3 rounded relative`} role={type}>
-            <p className={`text-${color}-500 flex flex-grow content-center align-middle justify-center`}>
-                <span>{message}</span>
+        <div className={`flex flex-row items-center justify-center w-full mb-5 ${bgColor} border ${borderColor} ${textColor} px-4 py-3 rounded relative`} role={type}>
+            <p className={`${textColor} flex flex-grow content-center align-middle justify-center`}>
+                {message}
             </p>
-            <button type="button" onClick={close} className={`cursor-pointer ml-2 text-${color}-500 w-5 h-5`}>
+            <button type="button" onClick={close} className={`cursor-pointer ml-2 ${textColor} w-5 h-5`}>
                 <CloseIcon className="w-5 h-5 " />
             </button>
         </div>
-    ) : null
+    ) : null
 }
